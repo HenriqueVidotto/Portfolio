66fd6b630b6427019de140dc7cf156fd453bffc4
Henrique Vidotto Vinico Neto <99729533+HenriqueVidotto@users.noreply.github.com>
2023-06-11 12:22:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function asp(){
+    var text;    
+ text = document.getElementById("texto");
+
+ text.innerHTML = "Conhecimento em desenvolvimento ASP.Net MVC";}
+
+
+function html()
+{
+    var text;    
+    text = document.getElementById("texto");
+   
+    text.innerHTML = "Conhecimento no desenvolvimento do frontEnd com html e css";
+}
+
+function java(){
+    var text;    
+    text = document.getElementById("texto");
+   
+    text.innerHTML = "Conhecimento com a linguagem java";
+}
+
+function javasript(){
+    var text;    
+    text = document.getElementById("texto");
+   
+    text.innerHTML = "Conhecimento na utilização de javascript em sites";
+}
+
+function C(){
+    var text;    
+    text = document.getElementById("texto");
+   
+    text.innerHTML = "Desenvolvimento de BackEnd com a liguagem C#";
+}
+
+function limpar(){
+    var text;    
+    text = document.getElementById("texto");
+   
+    text.innerHTML = "Clique ou coloque o mouse encima do icone";}
+   
